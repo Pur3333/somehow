742c4942f7f176f5d8d8c5cc84b0dea2662d1573
Pur3333 <lqw3rtyl@inbox.ru>
2023-11-24 00:28:04 +0300

    aaaa

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,62 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+    static void first_task (int arr_size) {
+        int[] arr = new int[arr_size];
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите элементы массива: ");
+        int sum = 0;
+        for (int i = 0; i < arr_size; i++) {
+            arr[i] = scan.nextInt();
+            sum += arr[i];
         }
+        double average = (double) sum / arr_size;
+        System.out.println("Сумма элементов массива: " + sum);
+        System.out.println("Среднее арифметическое элементов массива: " + average);
+    }
+    static void second_task(int arr_size) {
+        int[] arr = new int[arr_size];
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите элементы массива: ");
+        int sum = 0; int i = 0;
+        while(i < arr_size) {
+            arr[i] = scan.nextInt();
+            sum += arr[i];
+            i++;
+        }
+        i = 0; int min = 666; int max = 0;
+        do{
+            if (max < arr[i])
+                max = arr[i];
+            if (min > arr[i])
+                min = arr[i];
+            i++;
+        }while (i < arr_size);
+        System.out.println("Сумма элементов массива = " + sum + "\nМинимальный элемент массива: " +
+                min + "\nМаксимальный элемент: " + max);
+    }
+    static void third_task()
+    static void fourth_task() {
+        int i = 0, row = 0;
+        do {
+            row += 1/i;
+            i++;
+        } while(i < 10);
+        System.out.println(row);
+    }
+    static int fiveth_task(int num) {
+        if (num <= 1)
+            return 1;
+        return num * fiveth_task(num - 1);
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите количество элементов в массиве: ");
+        int n = scan.nextInt();
+        first_task(n);
+        second_task(n);
+        //third_task(output comm lines args)
+        fourth_task();
+        System.out.println(fiveth_task(5));
     }
 }
